e970cbed8fe8c858a8bc37af616e9ade976e5810
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-09 17:01:58 +0200

    Create a function "clear" to avoid repetition
    To avoid confusion, rename clear variable "clearbtn"

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const display = document.querySelector("#result");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 
-const clear = document.querySelector("#clear");
+const clearbtn = document.querySelector("#clear");
 
 numbers.forEach((number) => number.addEventListener("click", () => populateDisplay(number.textContent)));
 operators.forEach((operator) => operator.addEventListener("click", () => populateDisplay(operator.textContent)));
 
-clear.addEventListener("click", () => display.textContent = '');
+clearbtn.addEventListener("click", () => clear(display));
 
 
 function operate(number1, operator, number2) {
@@ -44,4 +44,8 @@ function divide(a, b) {
 
 function populateDisplay(number) {
     display.textContent += number;
+}
+
+function clear(div) {
+    div.textContent = '';
 }
